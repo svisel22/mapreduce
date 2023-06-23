7dada4c4015fa6f1541473c65eb0d639893dc851
svisel22 <s_visel22@stud.hwr-berlin.de>
2023-06-23 16:40:36 +0200

    Ex 5 reducer categories count above 114

--- reducer.py
+++ reducer.py
@@ -19,42 +19,60 @@ import sys
 # Visa  205.96
 # Cash  455.51
 
-# Sum of all sales (values) is initialized with zero, we just started
+
+
+
+
+count_threshold = 114
+previous_key = None
 count = 0
 
-# Previous key is initialized with None, we just started
-previous_key = None
-
-# For each new line in the standard input 
 for line in sys.stdin:
-
-    # split the line at the tabulator ("\t")
-    # strip removes whitespaces and new lines at the beginning and end of the line
-    # The result is a tuple with 2 elements
     data = line.strip().split("\t")
-
-    # Store the 2 elements of this line in seperate variables
     key = data[0]
 
-    # Do we have a previous_key (previous_key != None) and 
-    # is the new key different than the previous key?
-    # This means the line starts with a new key (key changes e.g. from "Visa" to "Cash")
-    # Remember that our keys are sorted
-    if previous_key != None and previous_key != key:
-        # Then write the result of the old key (Key=category, Value= Sum of Sales)
-        # to the standart output (stdout)
-        # Key and value are seperated by a tab (\t)
-        # Line ends with new line (\n)
-        sys.stdout.write("{0}\t{1}\n".format(previous_key, count))
-        # Reset 0
+    if previous_key is not None and previous_key != key:
+        if count > count_threshold:
+            sys.stdout.write("{0}\n".format(previous_key))
         count = 0
 
-    # Increment the count for each line
     count += 1
-    # Set the previous key for the next iteration
-    
     previous_key = key
 
-# write the last result the last key
-if previous_key is not None:
-    sys.stdout.write("{0}\t{1}\n".format(previous_key, count))
+if previous_key is not None and count > count_threshold:
+    sys.stdout.write("{0}\n".format(previous_key))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
