41f958f4aa978a691c13d63309f54f8e59df4263
svisel22 <s_visel22@stud.hwr-berlin.de>
2023-06-23 16:22:59 +0200

    Ex 3 mapper error where not 6 elements

--- mapper.py
+++ mapper.py
@@ -28,17 +28,23 @@ import sys
 
 # For each new line in the standard input (stdin) 
 for line in sys.stdin:
-
-    # split the line at the tabulator ("\t")
-    # strip removes whitespaces and new lines at the beginning and end of the line
-    # the result is a tuple with 6 elements
     data = line.strip().split("\t")
-
-    # store the 6 elements of the tuple in seperate variables
+    if len(data) != 6:
+        raise ValueError("Invalid input: Tuple does not have six elements")
     date, time, item, category, sales, payment = data
-
-    # Write the key-value combination to standard output (stdout)
-    # Key is the payment, value is the sales     
-    # With a tab (\t) between key and value
-    # New line \n means new record
     sys.stdout.write("{0}\t{1}\n".format(category, sales))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
